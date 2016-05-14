14c4a66ac6a49a4d0e0736feb9f536fc5c050787
DavidFleurantin <d.j.fleurantin@gmail.com>
2016-05-14 19:36:27 -0400

    Initial Commit

---
+++ GitTest/src/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author David
+ * Git Test
+ */
+public class GitTest {
+
+
+	public static void main(String[] args) {
+		
+		String message = "Hello Git!";
+		
+		System.out.println(message);
+
+	}
+
+}
