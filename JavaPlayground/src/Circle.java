13c032decc826aa3e029938fe93ecede7ef41ef1
DavidFleurantin <d.j.fleurantin@gmail.com>
2016-05-16 09:49:33 -0400

    Initial Commit

---
+++ JavaPlayground/src/Circle.java
@@ -0,0 +1,55 @@
+
+/* An instance (object) represents a circle */
+
+public class Circle {
+
+	// Instance Fields
+	public Double radius; // radius of the circle. radius >= 0
+
+	
+	/**
+	 * <dt><b>Constructor:</b><dd>
+	 * instance of Circle with radius r
+	 * 
+	 * <dt><b>Preconditions:</b><dd> 
+	 * r >= 0
+	 * 
+	 * @param r the radius of the circle
+	 */
+	public Circle(double r) {
+		radius = r;
+	}
+
+	// Methods
+
+	/**
+	 * Returns radius of specified Circle
+	 * 
+	 * @return radius of Circle
+	 */
+	public Double getRadius() {
+		return radius;
+	}
+
+	/**
+	 * Sets radius to new value 
+	 * <dt><b>Preconditions:</b><dd> 
+	 * r >= 0
+	 * 
+	 * @param r the newly specified radius
+	 */
+	public void setRadius(double r) {
+		assert r >= 0;
+		
+		radius = r;
+	}
+
+	/**
+	 * Returns the area of Circle
+	 * 
+	 * @return area of Circle
+	 */
+	public double area() {
+		return (Math.pow(radius, 2) * Math.PI);
+	}
+}
---
+++ JavaPlayground/src/Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * Precede every class with a comment
+ * 
+ * @author MSI
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		Circle myCircle = new Circle(5.0);
+		
+		myCircle.setRadius(10);
+
+		System.out.print(myCircle.area());
+
+	}
+
+}
